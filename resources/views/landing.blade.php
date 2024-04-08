4f8f5be34de0445fb1042b4a0f54dfe323dbde26
youssef <youssefhihi18@gmail.com>
2024-04-08 23:36:22 +0000

    frontend landing page 50%

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
 
@@ -12,13 +14,30 @@
 <section class="h-screen bg-gray-50 flex items-center">
 	<div class="w-full bg-cover bg-center py-32" style="background-image: url('https://source.unsplash.com/random');">
 		<div class="container mx-auto text-center text-white">
-			<h1 class="text-5xl font-medium mb-6">Now In Morroco</h1>
+			<h1 class="text-5xl font-medium font-normal mb-6">Now In Morroco</h1>
 			<p class="text-xl mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<a href="#" class="bg-indigo-500 text-white py-4 px-12 rounded-full hover:bg-indigo-600">Join Us</a>
+            <button class="relative px-8 py-2 rounded-xl bg-white isolation-auto z-10 border-2 border-red-500 text-red-600 hover:text-white font-normal font-semibold
+                    before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-red-600 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700">Join Us</button>                </div>
 		</div>
 	</div>
 </section>
+  
+<section class="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-10">
+    <div class="flex items-center justify-center w-full h-96 md:w-1/2">
+        <img class="object-cover w-full h-full max-w-2xl rounded-3xl" src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="apple watch photo">
+    </div>
+        <div class="max-w-lg md:mx-12 md:order-2">
+                <p class="mt-4 text-gray-600 text-xl ">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.
+                </p>
+                <div class="mt-6 flex justify-end">
+                </div>
+                </div>
 
+
+</section>
    
 
 
